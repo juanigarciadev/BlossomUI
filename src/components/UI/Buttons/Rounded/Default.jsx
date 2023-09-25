f88ece3f4c16bdc287472dfa42f5560f7e2bc290
juanigarciadev <juanigarciadev@gmail.com>
2023-09-24 21:40:53 -0300

    Fix: Modified 'a' to 'button' in buttons

--- src/components/UI/Buttons/Rounded/Default.jsx
+++ src/components/UI/Buttons/Rounded/Default.jsx
@@ -1,6 +1,6 @@
 export const PrimaryButtonRounded = () => {
     return (
-        <a href="" className='flex justify-center w-fit h-fit text-sm items-center text-center bg-black text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-neutral-800 dark:bg-white dark:text-black dark:hover:bg-neutral-200'>Primary button</a>
+        <button className='flex justify-center w-fit h-fit text-sm items-center text-center bg-black text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-neutral-800 dark:bg-white dark:text-black dark:hover:bg-neutral-200'>Primary button</button>
     )
 }
 
@@ -12,36 +12,36 @@ export const SecondaryButtonRounded = () => {
 
 export const DefaultButtonRounded = () => {
     return (
-        <a href="" className='flex justify-center w-fit h-fit text-sm items-center text-center bg-blue-700 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-blue-800'>Primary button</a>
+        <button className='flex justify-center w-fit h-fit text-sm items-center text-center bg-blue-700 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-blue-800'>Primary button</button>
     )
 }
 
 export const RedButtonRounded = () => {
     return (
-        <a href="" className='flex justify-center w-fit h-fit text-sm items-center text-center bg-red-700 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-red-800'>Primary button</a>
+        <button className='flex justify-center w-fit h-fit text-sm items-center text-center bg-red-700 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-red-800'>Primary button</button>
     )
 }
 
 export const GreenButtonRounded = () => {
     return (
-        <a href="" className='flex justify-center w-fit h-fit text-sm items-center text-center bg-green-700 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-green-800'>Primary button</a>
+        <button className='flex justify-center w-fit h-fit text-sm items-center text-center bg-green-700 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-green-800'>Primary button</button>
     )
 }
 
 export const YellowButtonRounded = () => {
     return (
-        <a href="" className='flex justify-center w-fit h-fit text-sm items-center text-center bg-yellow-400 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-yellow-500'>Primary button</a>
+        <button className='flex justify-center w-fit h-fit text-sm items-center text-center bg-yellow-400 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-yellow-500'>Primary button</button>
     )
 }
 
 export const PurpleButtonRounded = () => {
     return (
-        <a href="" className='flex justify-center w-fit h-fit text-sm items-center text-center bg-purple-600 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-purple-700 dark:bg-purple-700 dark:hover:bg-purple-800'>Primary button</a>
+        <button className='flex justify-center w-fit h-fit text-sm items-center text-center bg-purple-600 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-purple-700 dark:bg-purple-700 dark:hover:bg-purple-800'>Primary button</button>
     )
 }
 
 export const PinkButtonRounded = () => {
     return (
-        <a href="" className='flex justify-center w-fit h-fit text-sm items-center text-center bg-pink-700 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-pink-800'>Primary button</a>
+        <button className='flex justify-center w-fit h-fit text-sm items-center text-center bg-pink-700 text-white gap-2 px-4 py-3 rounded-full cursor-pointer font-medium select-none hover:bg-pink-800'>Primary button</button>
     )
 }
