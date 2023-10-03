453a481f8ab62f1649291b8b1d4a2ed50478f664
juanigarciadev <juanigarciadev@gmail.com>
2023-10-02 22:52:57 -0300

    Add: More information for Vite installation option

--- src/components/Docs/InstallationContent/ViteInstallation.jsx
+++ src/components/Docs/InstallationContent/ViteInstallation.jsx
@@ -3,23 +3,87 @@ import React from 'react'
 const ViteInstallation = () => {
     return (
         <div>
-            <section className='flex flex-col gap-6'>
-                <div className='grid grid-cols-2'>
-                    <section className='flex flex-col'>
-                        <h3 className='text-4xl font-bold pb-4 text-neutral-800 dark:text-white'>With Vite</h3>
-                        <h3 className='font-medium pb-2'>Create your project</h3>
-                        <p className='dark:text-neutral-300'>Start by creating a new Vite project if you don't have one set up already. The most common aproach is to use Create Vite.</p>
+            <h3 className='text-4xl font-bold pb-4 text-neutral-800 dark:text-white'>With Vite</h3>
+            <ol className='flex flex-col gap-2'>
+                <li className='grid grid-cols-2'>
+                    <section className='flex gap-2 '>
+                        <div className='flex flex-col items-center'>
+                            <p className='flex items-center justify-center w-6 h-6 bg-neutral-200 dark:bg-neutral-800 rounded-md text-sm'>1</p>
+                            <div className='w-[3px] h-full mt-2 bg-neutral-200 dark:bg-neutral-800'></div>
+                        </div>
+                        <div>
+                            <h3 className='font-medium pb-2'>Create your project</h3>
+                            <p className='dark:text-neutral-300 pb-4'>Start by creating a new Vite project if you don't have one set up already. The most common aproach is to use Create Vite.</p>
+                        </div>
                     </section>
                     <section className='flex flex-col'></section>
-                </div>
-                <div className='grid grid-cols-2'>
-                    <section className='flex flex-col'>
-                        <h3 className='font-medium pb-2'>Install Tailwind CSS</h3>
-                        <p className='dark:text-neutral-300'>Install `tailwindcss` and its peer dependencies, then generate your `tailwind.config.js` and `postcss.config.js` files.</p>
+                </li>
+                <li className='grid grid-cols-2'>
+                    <section className='flex gap-2 '>
+                        <div className='flex flex-col items-center'>
+                            <p className='flex items-center justify-center w-6 h-6 bg-neutral-200 dark:bg-neutral-800 rounded-md text-sm'>2</p>
+                            <div className='w-[3px] h-full mt-2 bg-neutral-200 dark:bg-neutral-800'></div>
+                        </div>
+                        <div>
+                            <h3 className='font-medium pb-2'>Install Tailwind CSS</h3>
+                            <p className='dark:text-neutral-300 pb-4'>Install `tailwindcss` and its peer dependencies, then generate your `tailwind.config.js` and `postcss.config.js` files.</p>
+                        </div>
                     </section>
                     <section className='flex flex-col'></section>
-                </div>
-            </section>
+                </li>
+                <li className='grid grid-cols-2'>
+                    <section className='flex gap-2 '>
+                        <div className='flex flex-col items-center'>
+                            <p className='flex items-center justify-center w-6 h-6 bg-neutral-200 dark:bg-neutral-800 rounded-md text-sm'>3</p>
+                            <div className='w-[3px] h-full mt-2 bg-neutral-200 dark:bg-neutral-800'></div>
+                        </div>
+                        <div>
+                            <h3 className='font-medium pb-2'>Configure your template paths</h3>
+                            <p className='dark:text-neutral-300 pb-4'>Add the paths to all of your template files in your `tailwind.config.js` file.</p>
+                        </div>
+                    </section>
+                    <section className='flex flex-col'></section>
+                </li>
+                <li className='grid grid-cols-2'>
+                    <section className='flex gap-2 '>
+                        <div className='flex flex-col items-center'>
+                            <p className='flex items-center justify-center w-6 h-6 bg-neutral-200 dark:bg-neutral-800 rounded-md text-sm'>4</p>
+                            <div className='w-[3px] h-full mt-2 bg-neutral-200 dark:bg-neutral-800'></div>
+                        </div>
+                        <div>
+                            <h3 className='font-medium pb-2'>Add the Tailwind directives to your CSS</h3>
+                            <p className='dark:text-neutral-300 pb-4'>Add the `@tailwind` directives for each of Tailwind’s layers to your `./src/index.css` file.</p>
+                        </div>
+                    </section>
+                    <section className='flex flex-col'></section>
+                </li>
+                <li className='grid grid-cols-2'>
+                    <section className='flex gap-2 '>
+                        <div className='flex flex-col items-center'>
+                            <p className='flex items-center justify-center w-6 h-6 bg-neutral-200 dark:bg-neutral-800 rounded-md text-sm'>5</p>
+                            <div className='w-[3px] h-full mt-2 bg-neutral-200 dark:bg-neutral-800'></div>
+                        </div>
+                        <div>
+                            <h3 className='font-medium pb-2'>Start your build process</h3>
+                            <p className='dark:text-neutral-300 pb-4'>Run your build process with `npm run dev`.</p>
+                        </div>
+                    </section>
+                    <section className='flex flex-col'></section>
+                </li>
+                <li className='grid grid-cols-2'>
+                    <section className='flex gap-2 '>
+                        <div className='flex flex-col items-center'>
+                            <p className='flex items-center justify-center w-6 h-6 bg-neutral-200 dark:bg-neutral-800 rounded-md text-sm'>5</p>
+                            <div className='w-[3px] h-full mt-2 bg-neutral-200 dark:bg-neutral-800'></div>
+                        </div>
+                        <div>
+                            <h3 className='font-medium pb-2'>Start using Tailwind in your project</h3>
+                            <p className='dark:text-neutral-300 pb-4'>Start using Tailwind’s utility classes to style your content.</p>
+                        </div>
+                    </section>
+                    <section className='flex flex-col'></section>
+                </li>
+            </ol>
         </div>
     )
 }
