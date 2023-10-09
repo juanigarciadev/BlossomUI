71f97840ba4cee5f94e7cc363c1dbc26683593b8
juanigarciadev <juanigarciadev@gmail.com>
2023-10-08 23:11:32 -0300

    Add: create Astro link

--- src/components/Docs/InstallationContent/AstroInstallation.jsx
+++ src/components/Docs/InstallationContent/AstroInstallation.jsx
@@ -24,7 +24,7 @@ const AstroInstallation = () => {
                                 <p className='flex items-center justify-center w-6 h-6 bg-neutral-200 dark:bg-neutral-800 rounded-md text-sm'>1</p>
                                 <h3 className='font-medium pb-2'>Create your project</h3>
                             </div>
-                            <p className='dark:text-neutral-300 pb-4'>Start by creating a new Astro project if you don't have one set up already. The most common aproach is to use create Astro.</p>
+                            <p className='dark:text-neutral-300 pb-4'>Start by creating a new Astro project if you don't have one set up already. The most common aproach is to use <a href="https://docs.astro.build/en/getting-started/#start-your-first-project" target='_blank' className='text-corporative hover:text-corporativeHover'>create Astro</a>.</p>
                         </div>
                     </section>
                     <section className='ml-2 xl:ml-8 shadow-md'>
