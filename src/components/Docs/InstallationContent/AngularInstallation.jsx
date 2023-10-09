47191a3710f5b4c3c4cd1abcf463a041b5956240
juanigarciadev <juanigarciadev@gmail.com>
2023-10-08 22:53:03 -0300

    Add: Angular CLI link

--- src/components/Docs/InstallationContent/AngularInstallation.jsx
+++ src/components/Docs/InstallationContent/AngularInstallation.jsx
@@ -26,7 +26,7 @@ const AngularInstallation = () => {
                                 <p className='flex items-center justify-center w-6 h-6 bg-neutral-200 dark:bg-neutral-800 rounded-md text-sm'>1</p>
                                 <h3 className='font-medium pb-2'>Create your project</h3>
                             </div>
-                            <p className='dark:text-neutral-300 pb-4'>Start by creating a new Angular project if you don't have one set up already. The most common aproach is to use Angular CLI.</p>
+                            <p className='dark:text-neutral-300 pb-4'>Start by creating a new Angular project if you don't have one set up already. The most common aproach is to use <a href="https://angular.io/cli" target='_blank' className='text-corporative hover:text-corporativeHover'>Angular CLI</a>.</p>
                         </div>
                     </section>
                     <section className='ml-2 xl:ml-8 shadow-md'>
