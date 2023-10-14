e3e1766f3cc076a490f0c33059be3ca56db70b56
juanigarciadev <juanigarciadev@gmail.com>
2023-10-13 22:15:21 -0300

    Fix: Mobile navbar z-index

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({ mobileNavToggle, setMobileNavToggle, moreDocs, setMoreDocs }) 
     const { theme, handleChangeTheme } = useContext(ThemeContext)
     return (
         <>
-            <header className='w-screen bg-white h-[70px] flex items-center justify-between z-10 fixed py-4 px-64 xl:px-32 md:px-16 sm:px-8 dark:bg-neutral-900 transition-colors duration-300'>
+            <header className='w-screen bg-white h-[70px] flex items-center justify-between z-20 fixed py-4 px-64 xl:px-32 md:px-16 sm:px-8 dark:bg-neutral-900 transition-colors duration-300'>
                 <Link className='font-bold text-xl text-neutral-900 dark:text-white' to="/">Blossom UI</Link>
                 <section className='flex items-center justify-center relative h-full w-2/4 xl:w-4/12 lg:hidden'>
                     <input type='text' placeholder='Search docs' className='flex items-center w-full h-full pl-10 placeholder:text-neutral-700 text-sm border border-neutral-300 rounded-lg shadow-md shadow-neutral-100 hover:shadow-neutral-200 duration-300 dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-sm dark:shadow-neutral-950 dark:hover:shadow-md dark:text-white dark:placeholder:text-neutral-300' />
