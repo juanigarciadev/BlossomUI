1d2960893dbc85fe86fbf2ae4a15d65f7a1074ac
juanigarciadev <juanigarciadev@gmail.com>
2023-10-17 22:37:23 -0300

    Fix: Footer now fixed in pages with little content

--- src/components/Docs/Changelog.jsx
+++ src/components/Docs/Changelog.jsx
@@ -9,22 +9,24 @@ const Changelog = () => {
     return (
         <div className='flex pt-[70px] w-full h-screen relative'>
             <Aside />
-            <section className='absolute left-[22%] w-[78%] pl-8 pt-4 xl:w-[82%] xl:left-[18%] lg:w-[100%] lg:left-0 lg:px-0 dark:text-white'>
-                <div className='flex gap-4 items-center pb-4'>
-                    <h1 className='text-4xl font-bold text-neutral-800 dark:text-white'>Changelog</h1>
-                    <div className='hidden items-center justify-center cursor-pointer h-8 w-8 rounded-lg bg-neutral-200 dark:bg-neutral-800 lg:flex' onClick={() => setDocNav(!docNav)}>
-                        <BiChevronDown />
-                    </div>
-                </div>
-                {docNav && (
-                    <DocNav />
-                )}
+            <section className='absolute left-[22%] w-[78%] h-[calc(100vh-70px)] flex flex-col justify-between pl-8 pt-4 xl:w-[82%] xl:left-[18%] lg:w-[100%] lg:left-0 lg:px-0 dark:text-white'>
                 <div>
-                    <h3 className='text-lg font-medium'>v1.0.0</h3>
-                    <span className='text-neutral-600 dark:text-neutral-500'>Released on October 9th, 2023.</span>
-                    <ol className='list-disc pt-4 pl-4'>
-                        <li className='text-neutral-600 dark:text-neutral-500'>Blossom UI now available, completely free and open source.</li>
-                    </ol>
+                    <div className='flex gap-4 items-center pb-4'>
+                        <h1 className='text-4xl font-bold text-neutral-800 dark:text-white'>Changelog</h1>
+                        <div className='hidden items-center justify-center cursor-pointer h-8 w-8 rounded-lg bg-neutral-200 dark:bg-neutral-800 lg:flex' onClick={() => setDocNav(!docNav)}>
+                            <BiChevronDown />
+                        </div>
+                    </div>
+                    {docNav && (
+                        <DocNav />
+                    )}
+                    <div>
+                        <h3 className='text-lg font-medium'>v1.0.0</h3>
+                        <span className='text-neutral-600 dark:text-neutral-500'>Released on October 9th, 2023.</span>
+                        <ol className='list-disc pt-4 pl-4'>
+                            <li className='text-neutral-600 dark:text-neutral-500'>Blossom UI now available, completely free and open source.</li>
+                        </ol>
+                    </div>
                 </div>
                 <Footer />
             </section>
--- src/components/Docs/Introduction.jsx
+++ src/components/Docs/Introduction.jsx
@@ -10,28 +10,30 @@ const Introduction = () => {
     return (
         <div className='flex pt-[70px] w-full h-screen relative'>
             <Aside />
-            <section className='absolute left-[22%] w-[78%] pl-8 pt-4 xl:left-[18%] lg:w-[100%] lg:left-0 lg:px-0 dark:text-white'>
-                <div className='flex gap-4 items-center pb-4'>
-                    <h1 className='text-4xl font-bold text-neutral-800 dark:text-white'>Introduction</h1>
-                    <div className='hidden items-center justify-center cursor-pointer h-8 w-8 rounded-lg bg-neutral-200 dark:bg-neutral-800 lg:flex' onClick={() => setDocNav(!docNav)}>
-                        <BiChevronDown />
+            <section className='absolute left-[22%] w-[78%] h-[calc(100vh-70px)] flex flex-col justify-between pl-8 pt-4 xl:left-[18%] lg:w-[100%] lg:left-0 lg:px-0 dark:text-white'>
+                <div>
+                    <div className='flex gap-4 items-center pb-4'>
+                        <h1 className='text-4xl font-bold text-neutral-800 dark:text-white'>Introduction</h1>
+                        <div className='hidden items-center justify-center cursor-pointer h-8 w-8 rounded-lg bg-neutral-200 dark:bg-neutral-800 lg:flex' onClick={() => setDocNav(!docNav)}>
+                            <BiChevronDown />
+                        </div>
                     </div>
+                    {docNav && (
+                        <DocNav />
+                    )}
+                    <section className='flex flex-col gap-2'>
+                        <p className='w-full'>Blossom UI is a library of components created with <a href="https://tailwindcss.com/" target='_blank' className='text-corporative cursor-pointer hover:text-corporativeHover'>Tailwind CSS</a>. Their use is completely free, the only requirement is to have Tailwind installed in the project.</p>
+                        <Link to="/docs/getting-started/installation" className='flex text-corporative cursor-pointer hover:text-corporativeHover'>How to install Tailwind CSS in my project<BiRightArrowAlt className='text-2xl' /></Link>
+                    </section>
+                    <section>
+                        <h2 className='text-2xl font-bold pt-6 text-neutral-800 dark:text-white'>How to use?</h2>
+                        <p>Very simple, just copy and paste our components code in your project.</p>
+                    </section>
+                    <section>
+                        <h2 className='text-2xl font-bold pt-6 text-neutral-800 dark:text-white'>How Tailwind works?</h2>
+                        <p>Tailwind CSS works by scanning all of your HTML files, JavaScript components, and any other templates for class names, generating the corresponding styles and then writing them to a static CSS file.</p>
+                    </section>
                 </div>
-                {docNav && (
-                    <DocNav />
-                )}
-                <section className='flex flex-col gap-2'>
-                    <p className='w-full'>Blossom UI is a library of components created with <a href="https://tailwindcss.com/" target='_blank' className='text-corporative cursor-pointer hover:text-corporativeHover'>Tailwind CSS</a>. Their use is completely free, the only requirement is to have Tailwind installed in the project.</p>
-                    <Link to="/docs/getting-started/installation" className='flex text-corporative cursor-pointer hover:text-corporativeHover'>How to install Tailwind CSS in my project<BiRightArrowAlt className='text-2xl' /></Link>
-                </section>
-                <section>
-                    <h2 className='text-2xl font-bold pt-6 text-neutral-800 dark:text-white'>How to use?</h2>
-                    <p>Very simple, just copy and paste our components code in your project.</p>
-                </section>
-                <section>
-                    <h2 className='text-2xl font-bold pt-6 text-neutral-800 dark:text-white'>How Tailwind works?</h2>
-                    <p>Tailwind CSS works by scanning all of your HTML files, JavaScript components, and any other templates for class names, generating the corresponding styles and then writing them to a static CSS file.</p>
-                </section>
                 <Footer />
             </section>
         </div>
