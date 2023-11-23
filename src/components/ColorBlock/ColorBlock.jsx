19a6bb29128ab4a3b58e5ad723d1a2cd8fa19ab8
juanigarciadev <juanigarciadev@gmail.com>
2023-11-22 23:31:48 -0300

    Refactor: Copied color tooltip in colors section

--- src/components/ColorBlock/ColorBlock.jsx
+++ src/components/ColorBlock/ColorBlock.jsx
@@ -11,9 +11,10 @@ const ColorBlock = ({ color, number, hex }) => {
             }, 1500)
         }}>
             {tooltip ? (
-                <span className='tooltip h-6 px-4 rounded-lg bg-neutral-200 absolute -top-9 text-xs font-medium z-10 flex items-center dark:bg-neutral-800 dark:darkTooltip' role='tooltip'>Copied!</span>
+                <span className='after:content-[""] after:w-[10px] after:h-[10px] after:bg-neutral-200 after:rotate-45 after:-bottom-[5px] after:left-8 after:absolute h-6 px-4 rounded-lg bg-neutral-200 absolute -top-9 text-xs font-medium z-10 flex items-center dark:bg-neutral-800 dark:after:bg-neutral-800' role='tooltip'>Copied!</span>
             ) :
-                null}
+                null
+            }
             <article className='flex flex-col w-16 cursor-pointer lg:w-12 xs:flex-row xs:w-full xs:items-center xs:gap-2 xxs:flex-col xxs:items-start'>
                 <div className={`${color} flex items-center justify-center w-full h-10 rounded-lg border lg:h-12 md:w-12`}></div>
                 <div className='flex flex-col'>
@@ -21,7 +22,7 @@ const ColorBlock = ({ color, number, hex }) => {
                     <span className='text-neutral-500 text-sm'>{hex}</span>
                 </div>
             </article>
-        </div>
+        </div >
     )
 }
 
