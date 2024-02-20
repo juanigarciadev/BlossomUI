e62a820db28190fd9bed0885b32fe59e5163cacf
Juan Ignacio Garcia <juanigarciadev@gmail.com>
2024-02-19 21:51:08 -0300

    Fix: 'Get started' and 'Explore components' not scrolling to top on click

--- src/components/ComponentsSection/ComponentsSection.jsx
+++ src/components/ComponentsSection/ComponentsSection.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
-import { BiLinkExternal } from 'react-icons/bi'
-import { Link } from 'react-router-dom'
-import { components } from '../../mocks/docs'
+import React from "react";
+import { BiLinkExternal } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { components } from "../../mocks/docs";
 
 const ComponentsSection = () => {
-    return (
-        <div className='lg:pt-32'>
-            <h3 className='text-6xl font-bold text-neutral-800 tracking-tight dark:text-white pb-8 text-center'>Components</h3>
-            <section className='grid grid-cols-3 gap-4 pb-32 md:flex lg:grid-cols-2 md:flex-col'>
-                {
-                    components.map((card) => {
-                        return (
-                            <Link to={card.url} key={card.name} className='flex flex-col border border-neutral-300 w-full h-auto rounded-lg shadow-sm hover:shadow-md duration-300 dark:border-transparent' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-                                <div className='flex items-center justify-between rounded-t-lg py-2 px-4 bg-neutral-300 dark:bg-neutral-800 dark:border-neutral-600 dark:text-white'>
-                                    <h3 className='font-bold'>{card.name}</h3>
-                                    <BiLinkExternal />
-                                </div>
-                                <hr />
-                                <img src={card.img} className='rounded-b-lg' alt={card.name} />
-                            </Link>
-                        )
-                    })
-                }
-            </section>
-        </div>
-    )
-}
+  return (
+    <div className="lg:pt-32">
+      <h3 className="text-6xl font-bold text-neutral-800 tracking-tight dark:text-white pb-8 text-center">
+        Components
+      </h3>
+      <section className="grid grid-cols-3 gap-4 pb-32 md:flex lg:grid-cols-2 md:flex-col">
+        {components.map((card) => {
+          return (
+            <Link
+              to={card.url}
+              key={card.name}
+              className="flex flex-col border border-neutral-300 w-full h-auto rounded-lg shadow-sm hover:shadow-md duration-300 dark:border-transparent"
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+              <div className="flex items-center justify-between rounded-t-lg py-2 px-4 bg-neutral-300 dark:bg-neutral-800 dark:border-neutral-600 dark:text-white">
+                <h3 className="font-bold">{card.name}</h3>
+                <BiLinkExternal />
+              </div>
+              <hr />
+              <img src={card.img} className="rounded-b-lg" alt={card.name} />
+            </Link>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
 
-export default ComponentsSection
+export default ComponentsSection;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,50 @@
-import React, { useEffect } from 'react'
-import { BiRightArrowAlt } from 'react-icons/bi'
-import { Link } from 'react-router-dom'
-import Explanation from '../Explanation/Explanation'
-import Characteristics from '../Characteristics/Characteristics'
-import ComponentsSection from '../ComponentsSection/ComponentsSection'
-import Footer from '../Footer/Footer'
-import ComponentsExample from '../ComponentsExample/ComponentsExample'
+import React, { useEffect } from "react";
+import { BiRightArrowAlt } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import Explanation from "../Explanation/Explanation";
+import Characteristics from "../Characteristics/Characteristics";
+import ComponentsSection from "../ComponentsSection/ComponentsSection";
+import Footer from "../Footer/Footer";
+import ComponentsExample from "../ComponentsExample/ComponentsExample";
 
 const Home = () => {
   useEffect(() => {
-    document.title = "Blossom UI - Components made with Tailwind"
-  }, [])
+    document.title = "Blossom UI - Components made with Tailwind";
+  }, []);
   return (
     <>
-      <main className='grid grid-cols-2 pt-64 lg:flex lg:flex-col lg:gap-12 home lg:pt-32'>
-
-        <section className='flex flex-col gap-6'>
-          <span id='badge-dismiss-default' className='flex items-center font-medium h-fit w-fit text-sm px-4 py-2 bg-neutral-100 text-neutral-900 rounded-full cursor-default dark:bg-neutral-700 dark:text-neutral-300'>Now with dark mode! 🌙</span >
-          <h1 className='text-6xl font-bold text-neutral-800 tracking-tight dark:text-white'>Your next project, in record time with <span className='text-corporative'>Blossom UI</span>.</h1>
-          <h3 className='text-lg text-neutral-700 dark:text-neutral-300'>Customizable, reusable and beautiful components made with Tailwind.</h3>
-          <section className='flex gap-4 sm:flex-col'>
-            <Link to="/docs/getting-started/introduction" className='flex justify-center items-center bg-black text-white w-fit h-fit text-sm gap-2 px-4 py-3 rounded-lg cursor-pointer font-medium select-none hover:bg-neutral-800 dark:bg-white dark:text-black dark:hover:bg-neutral-200 sm:w-full'>
-              Get started<BiRightArrowAlt className='text-2xl' /></Link>
-
-            <Link to="/components" className='flex justify-center items-center text-center border border-neutral-300 bg-transparent w-fit h-fit text-sm gap-2 px-4 py-[11px] rounded-lg cursor-pointer font-medium select-none hover:bg-neutral-200 dark:text-white dark:hover:bg-neutral-800'>Explore components<BiRightArrowAlt className='text-2xl' /></Link>
+      <main className="grid grid-cols-2 pt-64 lg:flex lg:flex-col lg:gap-12 home lg:pt-32">
+        <section className="flex flex-col gap-6">
+          <span
+            id="badge-dismiss-default"
+            className="flex items-center font-medium h-fit w-fit text-sm px-4 py-2 bg-neutral-100 text-neutral-900 rounded-full cursor-default dark:bg-neutral-700 dark:text-neutral-300"
+          >
+            Now with dark mode! 🌙
+          </span>
+          <h1 className="text-6xl font-bold text-neutral-800 tracking-tight dark:text-white">
+            Your next project, in record time with{" "}
+            <span className="text-corporative">Blossom UI</span>.
+          </h1>
+          <h3 className="text-lg text-neutral-700 dark:text-neutral-300">
+            Customizable, reusable and beautiful components made with Tailwind.
+          </h3>
+          <section className="flex gap-4 sm:flex-col">
+            <Link
+              to="/docs/getting-started/introduction"
+              className="flex justify-center items-center bg-black text-white w-fit h-fit text-sm gap-2 px-4 py-3 rounded-lg cursor-pointer font-medium select-none hover:bg-neutral-800 dark:bg-white dark:text-black dark:hover:bg-neutral-200 sm:w-full"
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+              Get started
+              <BiRightArrowAlt className="text-2xl" />
+            </Link>
+            <Link
+              to="/components"
+              className="flex justify-center items-center text-center border border-neutral-300 bg-transparent w-fit h-fit text-sm gap-2 px-4 py-[11px] rounded-lg cursor-pointer font-medium select-none hover:bg-neutral-200 dark:text-white dark:hover:bg-neutral-800"
+              onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+              Explore components
+              <BiRightArrowAlt className="text-2xl" />
+            </Link>
           </section>
         </section>
         <ComponentsExample />
@@ -33,7 +54,7 @@ const Home = () => {
       <ComponentsSection />
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
